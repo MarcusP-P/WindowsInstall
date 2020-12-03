25c77b4487b6a6c2abf84b658c017510dabbd5a5
Marcus Pallinger <git@marcus.pallinger.org>
2020-12-03 20:05:24 +1100

    Get more details

--- Tools/ConfigurationExtractor.ps1
+++ Tools/ConfigurationExtractor.ps1
@@ -34,46 +34,54 @@ $RawPath=$Path + "\Raw"
 
 New-Item -Path "$RawPath" -ItemType Directory | Out-Null
 
-Write-Host "Gettting Installed Programs"
+Write-Host "Getting Windows Edition"
+Get-WindowsEdition -Online| Out-String -Width 4096 | Out-File "$RawPath\WindowsEdition.txt"
+Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName WindowsEdition.txt
+
+Write-Host "Getting Installed Programs"
 Get-WmiObject -Class win32_product | Sort-Object -Property IdentifyingNumber | Out-String -Width 4096 | Out-File "$RawPath\InstalledPrograms.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName InstalledPrograms.txt
 
-Write-Host "Gettting AppX Bundles"
+Write-Host "Getting AppX Bundles"
 Get-AppxPackage -AllUsers -PackageTypeFilter Bundle | Select-Object Name,Publisher,Architecture,ResourceID,Version,PackageFullName,PublisherID | Sort-Object -Property PackageFullName | Out-String -Width 4096 | Out-File "$RawPath\AppXBundles.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName AppXBundles.txt
 
-Write-Host "Gettting AppX Frameworks"
+Write-Host "Getting AppX Frameworks"
 Get-AppxPackage -AllUsers -PackageTypeFilter Framework | Select-Object Name,Publisher,Architecture,ResourceID,Version,PackageFullName,PublisherID | Sort-Object -Property PackageFullName | Out-String -Width 4096 | Out-File "$RawPath\AppXFrameworks.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName AppXFrameworks.txt
 
-Write-Host "Gettting AppX Mains"
+Write-Host "Getting AppX Mains"
 Get-AppxPackage -AllUsers -PackageTypeFilter Main | Select-Object Name,Publisher,Architecture,ResourceID,Version,PackageFullName,PublisherID | Sort-Object -Property PackageFullName | Out-String -Width 4096 | Out-File "$RawPath\AppXMain.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName AppXMain.txt
 
-Write-Host "Gettting AppX Optional"
+Write-Host "Getting AppX Optional"
 Get-AppxPackage -AllUsers -PackageTypeFilter Optional | Select-Object Name,Publisher,Architecture,ResourceID,Version,PackageFullName,PublisherID | Sort-Object -Property PackageFullName | Out-String -Width 4096 | Out-File "$RawPath\AppXOptional.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName AppXOptional.txt
 
-Write-Host "Gettting AppX Resources"
+Write-Host "Getting AppX Resources"
 Get-AppxPackage -AllUsers -PackageTypeFilter Resource | Select-Object Name,Publisher,Architecture,ResourceID,Version,PackageFullName,PublisherID | Sort-Object -Property PackageFullName | Out-String -Width 4096 | Out-File "$RawPath\AppXResource.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName AppXResource.txt
 
-Write-Host "Gettting AppX Xaps"
+Write-Host "Getting AppX Xaps"
 Get-AppxPackage -AllUsers -PackageTypeFilter Xap | Select-Object Name,Publisher,Architecture,ResourceID,Version,PackageFullName,PublisherID | Sort-Object -Property PackageFullName | Out-String -Width 4096 | Out-File "$RawPath\AppXXaps.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName AppXXaps.txt
 
-Write-Host "Installed Drivers"
+Write-Host "Getting Installed Drivers"
 Get-WindowsDriver -Online -All | Sort-Object -Property OriginalFileName | Select-Object OriginalFileName,Inbox,ClassName,BootCritical,ProviderName,Date,Version | Out-String -Width 4096 | Out-File "$RawPath\Drivers.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName Drivers.txt
 
-Write-Host "Provisioned AppX Packages"
+Write-Host "Getting Provisioned AppX Packages"
 Get-AppxProvisionedPackage -online | Sort-Object -Property PackageName | Out-String -Width 4096 | Out-File "$RawPath\AppXProvisioned.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName AppXProvisioned.txt
 
-Write-Host "Windows Features"
+Write-Host "Getting Windows Features"
 Get-WindowsOptionalFeature -Online | Sort-Object -Property FeatureName | Out-String -Width 4096 | Out-File "$RawPath\WindowsFeatures.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName WindowsFeatures.txt
 
-Write-Host "Windows Capabilites"
+Write-Host "Getting Windows Capabilites"
 Get-WindowsCapability -Online | Sort-Object -Property Name | Out-String -Width 4096 | Out-File "$RawPath\WindowsCapabilities.txt"
 Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName WindowsCapabilities.txt
+
+Write-Host "Getting Windows Packages"
+Get-WindowsPackage -Online | Sort-Object -Property PackageName | Out-String -Width 4096 | Out-File "$RawPath\WindowsPackages.txt"
+Convert-LineEndings -SourcePath $RawPath -DestPath $Path -FileName WindowsPackages.txt
